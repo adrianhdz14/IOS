b6113a5c9f466eb1eec7fd97b8fd5a6f1083efe7
adrianhdz14 <adrianhdz1000@gmail.com>
2018-01-11 11:31:44 -0800

    Actividad 1

---
+++ Actividad 1/Playground01.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+//Adrian A. Flores Hdz 2700482
+
+import UIKit
+//variable
+var v1 = "esto es Swift"
+
+//asigna un numero
+let c1 = 4
+
+//represeta un dato tipo string
+var cadena:String = String()
+
+//representa la asignacion de dos datos
+cadena = "Hola mundo "  + v1
+
+//imprime
+print("El contenido es: \(cadena)")
+print("El valor de constante es: \(c1)")
+
+
